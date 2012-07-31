b0b2efc0a16bf7bb5f7f51de2be9eb3dc1daafcf
Paolo Capriotti <p.capriotti@gmail.com>
2012-07-31 23:58:53 +0100

    Factor out bashCompletionQuery.

--- Options/Applicative/BashCompletion.hs
+++ Options/Applicative/BashCompletion.hs
@@ -13,22 +13,22 @@ import Options.Applicative.Types
 bashCompletionParser :: Parser a -> Parser ParserFailure
 bashCompletionParser parser = complParser
   where
-    result ws i = failure $ case runCompletion compl ws i of
-      Nothing -> []
-      Just p  -> list_options p
-
     failure opts = ParserFailure
       { errMessage = \_ -> unlines opts
       , errExitCode = ExitSuccess }
-    compl = runParserWith pure parser
 
-    complParser = result
+    complParser = failure <$>
+      (   bashCompletionQuery parser
       <$> (many . strOption) (long "bash-completion-word")
-      <*> option (long "bash-completion-index")
+      <*> option (long "bash-completion-index") )
 
+bashCompletionQuery :: Parser a -> [String] -> Int -> [String]
+bashCompletionQuery parser ws i = case runCompletion compl ws i of
+  Nothing -> []
+  Just p  -> list_options p
+  where
     list_options = concat . mapParser (\_ -> map show_name . optionNames . optMain)
     show_name (OptShort c) = '-':[c]
     show_name (OptLong name) = "--" ++ name
 
-bashCompletionQuery :: Parser a -> [String] -> Int -> [String]
-bashCompletionQuery _ _ _ = []
+    compl = runParserWith pure parser
