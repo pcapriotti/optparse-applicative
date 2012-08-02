814e63fac367ab22b7308e6c103ebba1253cbc75
Paolo Capriotti <p.capriotti@gmail.com>
2012-08-02 20:19:14 +0100

    Add bash completer.

--- Options/Applicative/Builder/Completer.hs
+++ Options/Applicative/Builder/Completer.hs
@@ -3,6 +3,7 @@ module Options.Applicative.Builder.Completer
   , listCompleter
   , fileCompleter
   , dirCompleter
+  , bashCompleter
   ) where
 
 import Control.Applicative
@@ -10,6 +11,7 @@ import Control.Monad
 import Data.List
 import Options.Applicative.Types
 import System.Directory
+import System.Process
 
 listIOCompleter :: IO [String] -> Completer
 listIOCompleter ss = Completer $ \s ->
@@ -26,3 +28,9 @@ dirCompleter :: Completer
 dirCompleter = listIOCompleter $ do
   files <- getDirectoryContents "."
   filterM doesDirectoryExist files
+
+bashCompleter :: String -> Completer
+bashCompleter action = Completer $ \word -> do
+  let cmd = unwords ["compgen", "-A", action, word]
+  result <- readProcess "bash" ["-c", cmd] ""
+  return $ lines result
