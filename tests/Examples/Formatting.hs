356758af0cda6279dfc1e7ee35d9c588b0eaeb13
Paolo Capriotti <p.capriotti@gmail.com>
2013-10-20 18:12:32 +0100

    Test formatting of long metavariables.

--- tests/Examples/Formatting.hs
+++ tests/Examples/Formatting.hs
@@ -8,4 +8,5 @@ opts = option $ mconcat
   [ long "test"
   , short 't'
   , value 0
+  , metavar "FOO_BAR_BAZ_LONG_METAVARIABLE"
   , help "This is an options with a very very long description.  Hopefully, this will be nicely formatted by the help text generator." ]
