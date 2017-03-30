36ec30520cbdee77181c45830fd13cc31165e898
Huw Campbell <huw.campbell@ambiata.com>
2017-03-30 19:38:18 +1100

    Add zsh shell completion support.
    We also make fish shell's completion quite a lot nicer,
    with descriptions coming through well.

--- Options/Applicative/BashCompletion.hs
+++ Options/Applicative/BashCompletion.hs
@@ -17,6 +17,13 @@ import Options.Applicative.Builder
 import Options.Applicative.Common
 import Options.Applicative.Internal
 import Options.Applicative.Types
+import Options.Applicative.Help.Pretty
+import Options.Applicative.Help.Chunk
+
+data Richness
+  = Standard
+  | Enriched
+  deriving (Eq, Ord, Show)
 
 bashCompletionParser :: ParserInfo a -> ParserPrefs -> Parser CompletionResult
 bashCompletionParser pinfo pprefs = complParser
@@ -27,7 +34,8 @@ bashCompletionParser pinfo pprefs = complParser
     complParser = asum
       [ failure <$>
         (   bashCompletionQuery pinfo pprefs
-        <$> (many . strOption) (long "bash-completion-word"
+        <$> flag Standard Enriched (long "bash-completion-enriched" `mappend` internal)
+        <*> (many . strOption) (long "bash-completion-word"
                                   `mappend` internal)
         <*> option auto (long "bash-completion-index" `mappend` internal) )
       , failure <$>
@@ -36,14 +44,19 @@ bashCompletionParser pinfo pprefs = complParser
       , failure <$>
           (fishCompletionScript <$>
             strOption (long "fish-completion-script" `mappend` internal))
+      , failure <$>
+          (zshCompletionScript <$>
+            strOption (long "zsh-completion-script" `mappend` internal))
       ]
 
-bashCompletionQuery :: ParserInfo a -> ParserPrefs -> [String] -> Int -> String -> IO [String]
-bashCompletionQuery pinfo pprefs ws i _ = case runCompletion compl pprefs of
+bashCompletionQuery :: ParserInfo a -> ParserPrefs -> Richness -> [String] -> Int -> String -> IO [String]
+bashCompletionQuery pinfo pprefs richness ws i _ = case runCompletion compl pprefs of
   Just (Left (SomeParser p)) -> list_options p
   Just (Right c)             -> run_completer c
   _                          -> return []
   where
+    compl = runParserInfo pinfo (drop 1 ws')
+
     list_options
       = fmap concat
       . sequence
@@ -58,19 +71,48 @@ bashCompletionQuery pinfo pprefs ws i _ = case runCompletion compl pprefs of
     -- hinfoUnreachableArgs is off before running the
     -- completion for position arguments.
     opt_completions hinfo opt = case optMain opt of
-      OptReader ns _ _ -> return $ show_names ns
-      FlagReader ns _  -> return $ show_names ns
+      OptReader ns _ _ -> return . add_opt_help opt $ show_names ns
+      FlagReader ns _  -> return . add_opt_help opt $ show_names ns
       ArgReader rdr     | hinfoUnreachableArgs hinfo
                        -> return []
                         | otherwise
                        -> run_completer (crCompleter rdr)
-      CmdReader _ ns _  | hinfoUnreachableArgs hinfo
+      CmdReader _ ns p  | hinfoUnreachableArgs hinfo
                        -> return []
                         | otherwise
-                       -> return $ filter_names ns
+                       -> return . add_cmd_help p $ filter_names ns
+
+    -- When doing enriched completions, add any help specified
+    -- to the completion variables (tab separated).
+    add_opt_help :: Functor f => Option a -> f String -> f String
+    add_opt_help opt = case richness of
+      Standard
+        -> id
+      Enriched
+        -> fmap (\o -> let h = unChunk $ optHelp opt
+                       in  maybe o (\h' -> o ++ "\t" ++ render_line h') h)
+
+    -- When doing enriched completions, add the command description
+    -- to the completion variables (tab separated).
+    add_cmd_help :: Functor f => (String -> Maybe (ParserInfo a)) -> f String -> f String
+    add_cmd_help p = case richness of
+      Standard
+        -> id
+      Enriched
+        -> fmap (\cmd -> let h = p cmd >>= unChunk . infoProgDesc
+                         in  maybe cmd (\h' -> cmd ++ "\t" ++ render_line h') h)
 
     show_names :: [OptName] -> [String]
     show_names = filter_names . map showOption
+
+    -- We only want to show a single line in the completion results description.
+    -- If there's a line break, it will come across as a different completion
+    -- possibility.
+    render_line :: Doc -> String
+    render_line doc = case lines (displayS (renderPretty 0.4 80 doc) "") of
+      [] -> ""
+      [x] -> x
+      x : _ -> x ++ "..."
 
     filter_names :: [String] -> [String]
     filter_names = filter is_completion
@@ -85,8 +127,6 @@ bashCompletionQuery pinfo pprefs ws i _ = case runCompletion compl pprefs of
       case ws'' of
         w:_ -> isPrefixOf w
         _ -> const True
-
-    compl = runParserInfo pinfo (drop 1 ws')
 
 bashCompletionScript :: String -> String -> IO [String]
 bashCompletionScript prog progn = return
@@ -110,12 +150,6 @@ bashCompletionScript prog progn = return
 
 Derived from Drezil's post in #169.
 
-Fish shell's completions are quite simple, and although they do allow
-descriptions I can't see a way to apply them programatically (only one
-is allowed per call to complete, which in our case would apply the
-same description to all arguments).
-
-From fish manuals
 @
 commandline
 -c or --cut-at-cursor only print selection up until the current cursor position
@@ -126,13 +160,15 @@ We tokenize so that the call to count (and hence --bash-completion-index)
 gets the right number use cut-at-curstor to not bother sending anything
 after the cursor position, which allows for completion of the middle of
 words.
+
+Tab characters separate items from descriptions.
 -}
 fishCompletionScript :: String -> String -> IO [String]
 fishCompletionScript prog progn = return
   [ " function _" ++ progn
   , "    set -l cl (commandline --cut-at-cursor --tokenize)"
   , "    set -l cn (count $cl)"
-  , "    set -l tmpline --bash-completion-index $cn"
+  , "    set -l tmpline --bash-completion-enriched --bash-completion-index $cn"
   , "    for arg in $cl"
   , "      set tmpline $tmpline --bash-completion-word $arg"
   , "    end"
@@ -142,4 +178,38 @@ fishCompletionScript prog progn = return
   , "end"
   , ""
   , "complete --no-files --command " ++ progn ++ " --arguments '(_"  ++ progn ++  ")'"
+  ]
+
+zshCompletionScript :: String -> String -> IO [String]
+zshCompletionScript prog progn = return
+  [ "#compdef " ++ progn
+  , ""
+  , "local request"
+  , "local completions"
+  , "local word"
+  , ""
+  , "index=$((CURRENT - 1))"
+  , "request=(--bash-completion-enriched --bash-completion-index $index)"
+  , "for arg in ${words[@]}; do"
+  , "  request=(${request[@]} --bash-completion-word $arg)"
+  , "done"
+  , ""
+  , "IFS=$'\\n' completions=($( " ++ prog ++ " \"${request[@]}\" ))"
+  , ""
+  , "for word in $completions; do"
+  , "  local -a parts"
+  , ""
+  , "  # Split the line at a tab if there is one."
+  , "  IFS=$'\\t' parts=($( echo $word ))"
+  , ""
+  , "  if [[ -n $parts[2] ]]; then"
+  , "    # We have a description coming from the rich optparse"
+  , "    # implementation. Write its description in parentheses"
+  , "    # including the original word too (it's all that shown)."
+  , "    local desc=(\"$parts[1] ($parts[2])\")"
+  , "    compadd -d desc -- $parts[1]"
+  , "  else"
+  , "    compadd -f -- $word"
+  , "  fi"
+  , "done"
   ]
