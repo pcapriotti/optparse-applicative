7bf49778158ebdaf6d39b56f59ce3d882e1934e2
Huw Campbell <huw.campbell@gmail.com>
2019-07-19 13:58:27 +1000

    Print alternative with newline when it's nicer.
    If an Alternative case spreads over more that one line,
    we should probably not show a separate branch on the
    same line, as it's a bit hard to read.

--- src/Options/Applicative/Help/Pretty.hs
+++ src/Options/Applicative/Help/Pretty.hs
@@ -2,10 +2,11 @@ module Options.Applicative.Help.Pretty
   ( module Text.PrettyPrint.ANSI.Leijen
   , (.$.)
   , groupOrNestLine
+  , altSep
   ) where
 
 import           Control.Applicative
-import           Data.Monoid (mappend)
+import           Data.Semigroup ((<>))
 
 import           Text.PrettyPrint.ANSI.Leijen hiding ((<$>), (<>), columns)
 import           Text.PrettyPrint.ANSI.Leijen.Internal (Doc (..), flatten)
@@ -37,4 +38,19 @@ groupOrNestLine :: Doc -> Doc
 groupOrNestLine =
   Union
     <$> flatten
-    <*> ifNotAtRoot (mappend line) . nest 2
+    <*> ifNotAtRoot (line <>) . nest 2
+
+
+-- | Separate items in an alternative with a pipe.
+--
+--   If the first document and the pipe don't fit
+--   on the line, then mandatorily flow the next entry
+--   onto the following line.
+--
+--   The (<//>) softbreak ensures that if the document
+--   does fit on the line, there is at least a space,
+--   but it's possible for y to still appear on the
+--   next line.
+altSep :: Doc -> Doc -> Doc
+altSep x y =
+  group (x <+> char '|' <> line) <//> y
